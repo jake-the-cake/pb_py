a8c318f67a004212001f3dc9a4ce9af8791eafae
Jakey Wakey <askjake331@gmail.com>
2024-09-11 16:21:09 -0400

    numbers again

--- utils/number.py
+++ utils/number.py
@@ -1,72 +1,75 @@
-# from quiggle import Quiggle
+from utils.quiggle import Quiggle
 
-class Number:
+class Number(Quiggle):
 
 	def __init__(self, number: float | int | str, **kwargs):
+			
+			# inits
 		super().__init__()
-		self.value = number
 		self.options = {}
-		for key in	kwargs.keys(): self.options[key] = kwargs.get(key)
+		self._update_options(kwargs)
+			
+			# autovars
+		self.number_type = 'float'
+		self.original_value = number
+			
+			# set values
+		try: self._update_values(float(number))
+		except Exception as e: print('@ Number init ->', e)
 
+	def _update_values(self, value) -> None:
+		self.value = value
+		self.string_value = self._check_trailing_zeros(self.value)
+
+	def _update_options(self, options):
+		for key in options.keys(): self.options[key] = options.get(key)
+
+	def _check_trailing_zeros(self, value) -> str:
+		split_value = str(value).split('.')
+		if len(split_value) == 2:
+			v, val = split_value
+			diff = self.options.get('decimals') - len(val)
+			while diff > 0:
+				val += '0'
+				diff -= 1
+			value = '.'.join([v, val])
+		return value
+
+	def _round_number(self):
+			# vars for number of decimals and the rounding method
+		decimals = self.options.get('decimals', None)
+		rounding_method = self.options.get('round', 'auto')
+			# No rounding if decimals is not specified, or if needing trailing 0's
+		if decimals is None or decimals > self.get_decimal_length():
+			self._update_values(float(self.value))
+			return
+			# Apply rounding based on the specified method
+		if rounding_method == 'up':
+			factor = 10 ** decimals
+			self._update_values((int(self.value * factor) + 1) / factor)
+		elif rounding_method == 'down':
+			factor = 10 ** decimals
+			self._update_values(int(self.value * factor) / factor)
+		elif rounding_method == 'auto':
+			self._update_values(round(self.value, decimals))
+		else:
+			raise ValueError("Invalid rounding method. Choose 'up', 'down', or 'auto'.")
+
+		# callable methods
 	def make_int(self, **kwargs):
-		for key in	kwargs.keys(): self.options[key] = kwargs.get(key)
-		self.value = int(self.value)
+		self._update_options(kwargs)
+		self.number_type = 'int'
+		self.options['decimals'] = 0
+		self._round_number()
+		self._update_values(int(self.value))
 		return self
 	
 	def make_float(self, **kwargs):
-		for key in	kwargs.keys(): self.options[key] = kwargs.get(key)
-		self.value = float(self.value)
-		if 'decimals' in self.options:
-			val = str(self.value).split('.')
-			if len(val) == 2:
-				l_decimal = int(self.options['decimals'])
-				l_val = len(val[1])
-				diff = l_decimal - l_val
-				if diff > 0:
-					while diff > 0:
-						print(diff)
-						val[1] = val[1] + '0'
-						diff -= 1
-					self.value = '.'.join(val)
+		self._update_options(kwargs)
+		self.number_type = 'float'
+		self._round_number()
 		return self
-
-x = Number(88.25555, decimals = 3)
-x.make_float()
-print(x.value)
-
-# allowed_dots = 999
-
-# 	# verify that it can be converted
-# if dot_count > 1 or dot_count < 0: raise conversion_err
-
-# def check_whole_number_float(value: str):
-# 	split_value = value.split('.')
-# 	if (len(split_value) == 2):
-# 		for num in split_value[1]:
-# 			if num != '0': return
-# 		self.string_value = str(split_value[0])
-# 	if 'round_int' in self.parse_rules:
-# 		int_value = int(self.string_value.split('.')[0])
-# 		round_int_rule = self.parse_rules['round_int']
-# 		if round_int_rule == 'up':
-# 			self.value = int_value + 1
-# 			return
-# 		if round_int_rule == 'down':
-# 			self.value = int_value
-# 			return
-# 	raise format_err
-
-# check_whole_number_float(self.string_value)
-
-# 	# option to set output to float or int
-# if 'number_type' in self.parse_rules:
-# 	type = self.parse_rules['number_type']
-# 	if type == 'float': allowed_dots = 1
-# 	if type == 'int': allowed_dots = 0
-
-# 	# if there are no dots in the mumber, and its not specified as a float, make int
-# if dot_count == 0 and allowed_dots != 1: self.value = int(self.string_value)
-# 	# return a float if specified
-# elif allowed_dots == 1: self.value = float(self.string_value)
-# 	# if there is one dot, but specified as an int, raise error unless round_int option is valid
-# else: check_whole_number_float(self.string_value)
+	
+		# return the length of the decimal places for the value
+	def get_decimal_length(self) -> int:
+		return len(str(float(self.value)).split('.')[1])
